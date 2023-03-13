92bc881efbcf37de78069e391df6e0e793477c76
dnabanita7 <dashnabanita@gmail.com>
2023-03-13 11:29:20 +0530

    recognize stdeunts online

--- script.js
+++ script.js
@@ -1,56 +1,52 @@
-function onResults(results) {
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiFaceLandmarks) {
-    
-    for (const landmarks of results.multiFaceLandmarks) {
-      drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {color: PARAMS['FACE_COLOR'], lineWidth: 0.5});
-      drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: PARAMS['FACE_COLOR']});
-      drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: PARAMS['FACE_COLOR']});
-      drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: PARAMS['FACE_COLOR']});
-      drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: PARAMS['FACE_COLOR']});
-      drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: PARAMS['FACE_COLOR']});
-      drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: PARAMS['FACE_COLOR']});
-    }
-    // TODO: After drawing to the canvas we can send the detected face markings to the backend
-    // console.log(results.multiFaceLandmarks)
-  }
-  canvasCtx.restore();
+const imageUpload = document.getElementById('imageUpload')
+
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(start)
+
+async function start() {
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
+    })
+  })
 }
 
-// Grab relevant HTML elements
-const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
+function loadLabeledImages() {
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
 
-// Loading models
-async () => {
-await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
-await faceapi.loadFaceLandmarkModel(MODEL_URL)
-await faceapi.loadFaceRecognitionModel(MODEL_URL)
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
 }
-
-// Initiate face mesh and set the options based on the params file
-const faceMesh = new FaceMesh({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
-}});
-
-faceMesh.setOptions({
-  maxNumFaces: PARAMS['MAX_NUMBER_OF_FACES'],
-  refineLandmarks: PARAMS['REFINE_LANDMARKS'],
-  minDetectionConfidence: PARAMS['MIN_DETECTION_CONFIDENCE'],
-  minTrackingConfidence: PARAMS['MIN_TRACKING_CONFIDENCE']
-});
-faceMesh.onResults(onResults);
-  
-// Initiate camera
-const camera = new Camera(videoElement, {
-  onFrame: async () => {
-    await faceMesh.send({image: videoElement});
-    // video camera input
-  const results = await faceapi.detectAllFaces(video).withFaceLandmarks(true).withFaceDescriptors()
-  }
-});
-
-camera.start()
